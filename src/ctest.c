3253251d3d97f548ef6de8b09cc248990b8513ac
Dan Cristoloveanu <dcristo@microsoft.com>
2021-12-07 09:50:52 -0800

    Remove accidental vld.h include

--- src/ctest.c
+++ src/ctest.c
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include "vld.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
