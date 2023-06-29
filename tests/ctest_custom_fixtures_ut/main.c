01fb76a01c3469eedc358681291cae1140ed43b6
Dan Cristoloveanu <dcristo@microsoft.com>
2023-06-29 00:05:36 -0700

    New deps (#156)

--- tests/ctest_custom_fixtures_ut/main.c
+++ tests/ctest_custom_fixtures_ut/main.c
@@ -2,6 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stddef.h>  // for size_t
+
+#include "c_logging/logger.h"
+
 #include "ctest.h"
 
 #include "ctest_one_custom_fixture_ut.h"
@@ -10,6 +13,9 @@
 int main()
 {
     size_t failedTests = 0;
+
+    (void)logger_init();
+
     CTEST_RUN_TEST_SUITE(ctest_one_custom_fixture_ut);
     if (ctest_one_custom_fixture_test_succeeded() != 0)
     {
@@ -22,5 +28,8 @@ int main()
         // if the final validation of how we called the fixtures has failed it means at least one test is failed
         failedTests++;
     }
+
+    logger_deinit();
+
     return failedTests;
 }
--- tests/ctest_macro_hooks_ut/main.c
+++ tests/ctest_macro_hooks_ut/main.c
@@ -2,11 +2,20 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 #include <stddef.h>  // for size_t
+
+#include "c_logging/logger.h"
+
 #include "ctest.h"
 
 int main()
 {
     size_t failedTests = 0;
+
+    (void)logger_init();
+
     CTEST_RUN_TEST_SUITE(ctest_macro_hooks_ut);
+
+    logger_deinit();
+
     return failedTests;
 }
